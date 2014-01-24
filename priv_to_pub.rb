a6314484e0807dae00b448aa1f6e5dd06eba5387
tensaix2j <tensaix2j@gmail.com>
2014-01-24 19:57:38 +0800

    modified:   priv_to_pub.rb

--- priv_to_pub.rb
+++ priv_to_pub.rb
@@ -6,7 +6,7 @@ def main( argv )
 	if argv.length > 0 
 		puts bitcoin_priv_to_pub argv[0]
 	else
-		printf "Usage ruby %s <privatekey>", __FILE__
+		printf "Usage : ruby %s <privatekey>\n", __FILE__
 	end	
 end
 
