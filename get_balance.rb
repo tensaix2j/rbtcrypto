a6604d3c568964df5e5857b004b4a8fb273a7270
tensaix2j <tensaix2j@gmail.com>
2014-03-11 20:22:43 +0800

    modified:   get_balance.rb

--- get_balance.rb
+++ get_balance.rb
@@ -8,10 +8,10 @@ def main( argv )
 		if bitcoin_verify_pubkey( argv[0] )  == true
 			puts get_balance( argv[0] )
 		else
-			puts "Invalid public key #{argv[0]}"
+			puts "Invalid public address #{argv[0]}"
 		end
 	else
-		printf "Usage : ruby %s <public key>\n", __FILE__
+		printf "Usage : ruby %s <public address>\n", __FILE__
 	end	
 end
 
